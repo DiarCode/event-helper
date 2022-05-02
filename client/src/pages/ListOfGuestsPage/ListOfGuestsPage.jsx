645c1c3be84bcff130af21dbedac9f7d10fb71ea
DiarCode <diar_beg64@mail.ru>
2022-05-02 23:47:09 +0600

    guest list filter

--- client/src/pages/ListOfGuestsPage/ListOfGuestsPage.jsx
+++ client/src/pages/ListOfGuestsPage/ListOfGuestsPage.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import Footer from "../../components/Footer/Footer";
 import GuestCard from "../../components/GuestCard/GuestCard";
@@ -9,8 +10,30 @@ import "./listOfGuestsPage.css";
 
 const ListOfGuestsPage = () => {
   const { id } = useParams();
+  const [searchValue, setSearchValue] = useState("");
   const guestsList = useFetchGuestsList(id);
+  const [filteredList, setFilteredList] = useState();
   const createNewGuestURL = `/create-guest/${id}`;
+
+  useEffect(() => {
+    if (searchValue == "") {
+      setFilteredList(() => guestsList);
+    }
+  }, [guestsList, filteredList]);
+
+  function handleSearchInput(e) {
+    setSearchValue(() => e.target.value);
+
+    setFilteredList(
+      filteredList.filter(guest => {
+        if (searchValue == "") return guest;
+        return (
+          guest.guestName.toLowerCase().includes(searchValue.toLowerCase()) ||
+          guest.guestSeat.includes(searchValue)
+        );
+      })
+    );
+  }
 
   return (
     <div className="guestList">
@@ -22,14 +45,22 @@ const ListOfGuestsPage = () => {
         </Link>
 
         <div className="guestList-search">
-          <input type="text" placeholder="Enter name of guest" />
+          <input
+            type="text"
+            placeholder="Enter name or seat"
+            value={searchValue}
+            onChange={handleSearchInput}
+          />
         </div>
 
         <div className="guestList-list">
-          {guestsList &&
-            guestsList.map(guest => (
+          {filteredList && filteredList.length !== 0 ? (
+            filteredList.map(guest => (
               <GuestCard info={guest} key={generateKey(guest.guestName)} />
-            ))}
+            ))
+          ) : (
+            <div className="guestList-list-handler">There's nothing here</div>
+          )}
         </div>
       </div>
       <Footer />
